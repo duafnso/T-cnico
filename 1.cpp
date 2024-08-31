4fb05448ecd3b651654137509dd724c52fd6a965
duafnso <145731071+duafnso@users.noreply.github.com>
2024-08-31 17:44:27 -0300

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+int INDICE = 13, SOMA = 0, K = 0;
+
+Enquanto K < INDICE faça {
+   K = K + 1;
+   SOMA = SOMA + K;
+}
+
+Imprimir(SOMA);
+
+Esse código incrementa o valor de K até que ele atinja o valor de INDICE (13). A cada iteração, K é somado à variável SOMA. O resultado final de SOMA será a soma de todos os números de 1 a 13.
+Portanto, o valor final da variável SOMA será 91
